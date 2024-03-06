8f4aac13c123a0c6ac7e973daf0b15002a27ea68
Virtinandu <146226389+Virtinandu@users.noreply.github.com>
2024-03-06 05:32:23 -0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
 import streamlit as st
-def main():
-    # Sidebar options
-    st.sidebar.title("Index")
-    page = st.sidebar.selectbox("Select a page", ["Home", "Subjects", "About"])
 
-    if page == "Home":
-        display_home_page()
-    elif page == "Subjects":
-        display_subjects_page()
-    elif page == "About":
-        display_about_page()
+# Set page title and favicon
+st.set_page_config(page_title="Study Material", page_icon=":books:", layout="wide")
 
-def display_home_page():
-    st.header("Welcome to Brain Bloom")
-    st.header_alignment=('center')
-def display_subjects_page():
-    st.header("Subjects")
-    subjects = ["Python", "Android", "Java", "Computer Networks"]
+# Add a CSS file to style the app
+st.markdown("""
+<style>
+.big-font {
+    font-size: 30px;
+    color: #3F84B3;
+}
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    margin: 10px;
+    padding: 10px;
+}
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+</style>
+""", unsafe_allow_html=True)
 
-    selected_subject = st.selectbox("Select a subject", subjects)
-
-    if selected_subject == "Python":
-        display_Python_material()
-    elif selected_subject == "Android":
-        display_Android_material()
-    elif selected_subject == "Java":
-        display_Java_material()
-    elif selected_subject == "Computer Networks":
-        display_Computer_Networks_material()
-
-def display_Python_material():
-    st.subheader("Python Study Material")
-
-def display_Android_material():
-    st.subheader("Android Study Material")
-
-def display_Java_material():
-    st.subheader("Java Study Material")
-
-def display_Computer_Networks_material():
-    st.subheader("Computer Networks Study Material")
-
-def display_about_page():
-    st.header("About")
-    st.write("This website is created using Streamlit, a popular Python library for building web apps.")
-
-
-if __name__ == "__main__":
-    main()
+# Add study material as markdown links
+st.write("<h1 class='big-font'>Study Material</h1>", unsafe_allow_html=True)
+st.write("<a class='card' href='https://www.oreilly.com/library/view/python-cookbook-3rd/9781449357332/' target='_blank'>Python Cookbook</a>", unsafe_allow_html=True)
+st.write("<a class='card' href='https://automatetheboringstuff.com/' target='_blank'>Automate the Boring Stuff</a>", unsafe_allow_html=True)
+st.write("<a class='card' href='https://docs.scipy.org/doc/numpy/reference/' target='_blank'>NumPy Reference</a>", unsafe_allow_html=True)
+st.write("<a class='card' href='https://pandas.pydata.org/docs/' target='_blank'>Pandas Documentation</a>", unsafe_allow_html=True)
+st.write("<a class='card' href='https://matplotlib.org/stable/contents.html' target='_blank'>Matplotlib Documentation</a>", unsafe_allow_html=True)
